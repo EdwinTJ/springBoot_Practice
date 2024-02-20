6705c9fd6e2a3486b65dbdcc64cd2ba072f783cd
Edwin Silvestre <a02305847@aggies.usu.edu>
2024-02-20 09:43:41 -0700

    CrossOrigin

--- src/main/java/cmsSoftware/cmsSchool/student/StudentController.java
+++ src/main/java/cmsSoftware/cmsSchool/student/StudentController.java
@@ -5,11 +5,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 public class StudentController {
 
     @Autowired
     private StudentRepository studentRepository;
+
 
     @GetMapping("/students")
     List<StudentModel> getAllStudents() {
